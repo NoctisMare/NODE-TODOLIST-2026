6c5ad8e274188b6f1c8825abfd26222ff3a37030
lucasSterlings <lucas_sterling_orginal@proton.me>
2026-09-15 05:37:08 +0000

    feat: model schema was created.

---
+++ src/model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+export const STATUS = Object.freeze({
+  COMPLETED: "completed",
+  PENDING: "pending",
+});
+
+const transformFunction = (doc, ret) => {
+  const { __v } = ret || {};
+  return ret;
+};
+
+const todoListSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: Object.values(STATUS),
+      default: STATUS.PENDING,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: transformFunction,
+    },
+    toObject: {
+      virtuals: true,
+      transform: transformFunction,
+    },
+  },
+);
+
+const TodoList = mongoose.model("TodoList", todoSchema);
+
+export default TodoList;
